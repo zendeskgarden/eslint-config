554e2de2e2ccfb2455008c592de67d4de00bc10c
Jonathan Zempel <jzempel@gmail.com>
2018-03-12 17:43:47 +0800

    Add comments

--- plugins/notice.js
+++ plugins/notice.js
@@ -8,8 +8,10 @@ const TEMPLATE = `/**
 `;
 
 module.exports = {
+  // https://www.npmjs.com/package/eslint-plugin-notice
   plugins: ['notice'],
   rules: {
+    // throw an error when a file doesn't have a copyright notice
     'notice/notice': [2, { template: TEMPLATE }]
   }
 }
