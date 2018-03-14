0c310590121085b18e715c87cfff5b0f8dd507e8
Jonathan Zempel <jzempel@gmail.com>
2018-03-14 12:00:18 +0800

    Modify copyright header

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * Copyright Zendesk, Inc.
  *
  * Use of this source code is governed under the Apache License, Version 2.0
- * found in the LICENSE file located in the root directory of this source tree.
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
+
 
 module.exports = {
   extends: [
--- plugins/notice.js
+++ plugins/notice.js
@@ -2,7 +2,7 @@ const TEMPLATE = `/**
  * Copyright Zendesk, Inc.
  *
  * Use of this source code is governed under the Apache License, Version 2.0
- * found in the LICENSE file located in the root directory of this source tree.
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
 `;
@@ -11,6 +11,9 @@ module.exports = {
   plugins: ['notice'],
   rules: {
     // throw an error when a file doesn't have a copyright notice
-    'notice/notice': [2, { template: TEMPLATE }]
+    'notice/notice': [2, {
+      template: TEMPLATE,
+      onNonMatchingHeader: 'replace'
+    }]
   }
 };
