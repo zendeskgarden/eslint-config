7f6c2cf27bc558a354b2f1146522457d41baec9e
Mike Tobia <mike.tobia@zendesk.com>
2023-07-06 12:57:39 -0400

    fix: add rules

--- plugins/typescript-semantics.js
+++ plugins/typescript-semantics.js
@@ -23,6 +23,8 @@ module.exports = {
 
     // disallows awaiting a value that is not a `Thenable`
     '@typescript-eslint/await-thenable': 2,
+    // suggest `NonNullable<unknown>` as an alternative for `{}` and `Object`
+    '@typescript-eslint/ban-types': 1,
     // enforce dot notation whenever possible
     '@typescript-eslint/dot-notation': bestPractices['dot-notation'],
     // enforce consistent spacing between keys and values in object literal properties
@@ -31,6 +33,8 @@ module.exports = {
     '@typescript-eslint/no-base-to-string': 2,
     // requires expressions of type void to appear in statement position
     '@typescript-eslint/no-confusing-void-expression': 2,
+    // disallows duplicate union or intersection type members
+    '@typescript-eslint/no-duplicate-type-constituents': 2,
     // requires Promise-like values to be handled appropriately
     '@typescript-eslint/no-floating-promises': 2,
     // disallow iterating over an array with a for-in loop
@@ -60,6 +64,8 @@ module.exports = {
     '@typescript-eslint/no-unsafe-assignment': 2,
     // disallows calling an any type value
     '@typescript-eslint/no-unsafe-call': 2,
+    // disallow literals where enums are expected
+    '@typescript-eslint/no-unsafe-enum-comparison': 2,
     // disallows member access on any typed variables
     '@typescript-eslint/no-unsafe-member-access': 2,
     // disallows returning any from a function
