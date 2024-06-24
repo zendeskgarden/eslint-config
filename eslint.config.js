95c5efef7a02caf33bc0affc72c9f516507ec477
Jonathan Zempel <jzempel@gmail.com>
2024-06-24 16:56:50 -0400

    Set up for deploy

--- eslint.config.js
+++ eslint.config.js
@@ -29,9 +29,13 @@ export default [
   },
   {
     rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off'
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      'jest/require-hook': 'off'
     }
   }
 ];
