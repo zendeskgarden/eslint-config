6d8bf95b42034c0dd98f5261ce636dcaa912def5
Zendesk Garden <garden@zendesk.com>
2024-09-12 13:46:35 +0000

    Updates [skip ci]

---
+++ _nuxt/B703NL64.js
@@ -0,0 +1,1 @@
+import{f as o,i as r,c as t,W as n,o as s}from"./BsYh-B0w.js";const p=o({__name:"index",setup(a){return r().replace("/configs"),(e,l)=>(s(),t("div",null,[n(e.$slots,"default")]))}});export{p as default};
