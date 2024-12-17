33f5d72c3551a60128e27c09cf53be9dd6ed8ad8
Zendesk Garden <garden@zendesk.com>
2024-12-17 21:19:25 +0000

    Updates [skip ci]

---
+++ _nuxt/CKk-673z.js
@@ -0,0 +1,1 @@
+import{e as o,h as r,c as t,U as n,o as s}from"./DcxSLaLk.js";const p=o({__name:"index",setup(a){return r().replace("/configs"),(e,l)=>(s(),t("div",null,[n(e.$slots,"default")]))}});export{p as default};
